1c8ae7d2f6571b971ee29db7da91ab897e89edc2
aaronamm <theeranitp@gmail.com>
2019-10-15 08:17:22 +0700

    Correct query, wrong relationship between tables

--- CodeSanook.StackOverflowEFQuery.ConsoleApp/Program.cs
+++ CodeSanook.StackOverflowEFQuery.ConsoleApp/Program.cs
@@ -11,42 +11,39 @@ namespace CodeSanook.StackOverflowEFQuery.ConsoleApp
         public static void Main(string[] args)
         {
             // Converted from https://data.stackexchange.com/stackoverflow/query/785/how-many-upvotes-do-i-have-for-each-tag
+
+            byte voteType = 2;
+            int userId = 55;
             using (var db = new StackOverflowDbContext())
-            using (var transaction = db.Database.BeginTransaction())
             {
-                byte voteType = 2;
-                int userId = 55;
-                var query1 = from t in db.Tags
-                            from pt in t.PostTags
-                            let p = pt.Post
-                            join v in db.Votes on new { PostId = p.Id, VoteTypeId = voteType } equals new { v.PostId, v.VoteTypeId }
-                            where p.OwnerUserId == userId
-                            group t by t.TagName into g
-                            orderby g.Count()
-                            select new { TagName = g.Key, Upvotes = g.Count() };
-
-
-                var query2 = from t in db.Tags
+                var query = from t in db.Tags
                             join pt in db.PostTags on t.Id equals pt.TagId
-                            join p in db.Posts on pt.PostId equals p.Id
-                            join v in db.Votes on new { PostId = p.Id,  VoteTypeId  = voteType} equals new { v.PostId, v.VoteTypeId }
+                            join p in db.Posts on pt.PostId equals p.ParentId
+                            join v in db.Votes on new { PostId = p.Id, VoteTypeId = voteType }
+                            equals new { v.PostId, v.VoteTypeId }
                             where p.OwnerUserId == userId
                             group t by t.TagName into g
-                            orderby g.Count()
                             select new { TagName = g.Key, Upvotes = g.Count() };
 
-                foreach (var item in query1.ToList())
+                query = query.OrderBy(t => t.Upvotes);
+
+                foreach (var item in query.ToList())
                 {
                     Console.WriteLine($"{item.TagName} {item.Upvotes}");
                 }
 
-                foreach (var item in query2.ToList())
-                {
-                    Console.WriteLine($"{item.TagName} {item.Upvotes}");
-                }
-
-                transaction.Commit();
             }
         }
     }
 }
+
+//alternatively
+//var query = from t in db.Tags
+//             from pt in t.PostTags
+//             let p = pt.Post
+//             join v in db.Votes on new { PostId = p.Id, VoteTypeId = voteType } equals new { v.PostId, v.VoteTypeId }
+//             where p.OwnerUserId == userId
+//             group t by t.TagName into g
+//             orderby g.Count()
+//             select new { TagName = g.Key, Upvotes = g.Count() };
+
